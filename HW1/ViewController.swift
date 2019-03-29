5aff488a3133f2bd588f1c24863b9a9cf44f9007
“KaiHao” <okh8609@gmail.com>
2019-03-29 02:55:23 -0700

    Initial Commit

---
+++ HW1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW1
+//
+//  Created by KaiHao  on 2019/3/29.
+//  Copyright © 2019 NTUST. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
